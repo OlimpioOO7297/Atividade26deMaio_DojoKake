ec3d9c166f129595cd569479e07edc850cfb8870
OlimpioOO7297 <wesley_wed@hotmail.com>
2016-05-13 14:36:24 -0300

    Update Principal.java

--- Everton/AtividadePraticaIX/src/br/unifor/cct/cc/logica/pratica/Principal.java
+++ Everton/AtividadePraticaIX/src/br/unifor/cct/cc/logica/pratica/Principal.java
@@ -24,6 +24,10 @@ public class Principal {
 			case 2:
 				verificarCandidato(numeroInscricao, candidatos);
 				break;
+				
+			case 5: 
+				finalizaPrograma();
+				break;
 			default:
 				break;
 			}
@@ -173,4 +177,8 @@ public class Principal {
 	}
 	
 	// Questão 08 - Francisco Olímpio / Gabriel Albuquerque
+	public static void finalizaPrograma(){
+		System.out.println("Programa encerrado!");
+		System.exit(0);
+	}
 }
